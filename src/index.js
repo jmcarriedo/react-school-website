6a90f40599e82fa8d959924d8e85dd9201309f6c
jmcarriedo <87625886+jmcarriedo@users.noreply.github.com>
2021-08-26 09:26:10 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Info from './components/Landing Page/Info';
 import About from './pages/about';
 import Rooms from './pages/rooms';
 import Programs from './pages/programs';
@@ -9,24 +10,23 @@ import Admissions from './pages/admissions';
 import Contact from './pages/contact';
 import Login from './pages/login';
 import Shop from './pages/shop';
-import Home from './pages/index';
+import Home from './pages/home';
 
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path='/index' exact component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/rooms' component={Rooms} />
-      <Route path='/programs' component={Programs} />
-      <Route path='/educators' component={Educators} />
-      <Route path='/admissions' component={Admissions} />
-      <Route path='/shop' component={Shop} />
-      <Route path='/contact' component={Contact} />
-      <Route path='/login' component={Login} />
-    </Switch>
-	</Router>,
+  <Router >
+      <Switch>
+          <Route path='/home' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/rooms' component={Rooms} />
+          <Route path='/programs' component={Programs} />
+          <Route path='/educators' component={Educators} />
+          <Route path='/admissions' component={Admissions} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/login' component={Login} />
+      </Switch>
+  </Router>,
   document.getElementById('root')
 );
-
 
