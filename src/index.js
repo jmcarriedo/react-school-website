3bc2d646c64ac29d31854b2d379e72c7e3ed857f
jmcarriedo <87625886+jmcarriedo@users.noreply.github.com>
2021-08-27 10:34:43 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import About from './pages/about';
-import Rooms from './pages/rooms';
-import Programs from './pages/programs';
-import Educators from './pages/educators';
-import Admissions from './pages/admissions';
-import Contact from './pages/contact';
-import Login from './pages/login';
-import Shop from './pages/shop';
-import Home from './pages/index';
-
+import App from './App';
 
 ReactDOM.render(
-  <Router basename="/react-school-website">
-      <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/rooms' component={Rooms} />
-          <Route path='/programs' component={Programs} />
-          <Route path='/educators' component={Educators} />
-          <Route path='/admissions' component={Admissions} />
-          <Route path='/shop' component={Shop} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/login' component={Login} />
-      </Switch>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
