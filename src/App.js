c58ea34987a1951b70f4e977aa6b81e6b9c165f4
jmcarriedo <87625886+jmcarriedo@users.noreply.github.com>
2021-08-31 10:13:57 +0800

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     return (
         <>
         <Router>
+            <Sidebar />
             <Switch>
                 <Route path='/index' exact component={Index} />
                 <Route path='/about' component={About} />
