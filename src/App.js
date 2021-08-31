5bb57bb1dcbf5449df0ea3850773f1af1cd76b45
jmcarriedo <87625886+jmcarriedo@users.noreply.github.com>
2021-08-31 17:23:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ const App = () => {
             <Sidebar />
             <Switch>
                 <Route path='/' exact component={Home} />
-                <Route path='/about' exact component={About} />
-                <Route path='/programs' component={Programs} />
-                <Route path='/educators' component={Educators} />
-                <Route path='/admissions' component={Admissions} />
-                <Route path='/shop' component={Shop} />
-                <Route path='/contact' component={Contact} />
-                <Route path='/login' component={Login} />
+                <Route path='/react-school-website/about' exact component={About} />
+                <Route path='/react-school-website/programs' component={Programs} />
+                <Route path='/react-school-website/educators' component={Educators} />
+                <Route path='/react-school-website/admissions' component={Admissions} />
+                <Route path='/react-school-website/shop' component={Shop} />
+                <Route path='/react-school-website/contact' component={Contact} />
+                <Route path='/react-school-website/login' component={Login} />
             </Switch>
         </Router>
         </>
